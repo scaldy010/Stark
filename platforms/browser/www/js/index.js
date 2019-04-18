fb3f1e896af139acbeddda20ae9ff7526d7d079f
scaldy010 <47572824+scaldy010@users.noreply.github.com>
2019-04-18 14:02:43 +0100

    have working location but not sure why?

--- platforms/browser/www/js/index.js
+++ platforms/browser/www/js/index.js
@@ -1,49 +1,127 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-var app = {
-    // Application Constructor
-    initialize: function() {
-        this.bindEvents();
-    },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-    },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicitly call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
-        app.receivedEvent('deviceready');
-    },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
+// Initialize app
+var myApp = new Framework7();
 
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
 
-        console.log('Received Event: ' + id);
+
+var $$ = Dom7;
+
+
+var mainView = myApp.addView('.view-main', {
+    // Because we want to use dynamic navbar, we need to enable it for this view:
+    dynamicNavbar: true
+});
+
+// Handle Cordova Device Ready Event
+$$(document).on('deviceready', function() {
+    console.log("Device is ready!");
+
+});
+
+// This function is going to use the plugin to 
+// get the latitude and longitud from the device
+function getLocation(){
+    // Once the position has been retrieved, an JSON object
+    // will be passed into the callback function (in this case geoCallback)
+    // If something goes wrong, the onError function is the 
+    // one that will be run
+    navigator.geolocation.getCurrentPosition(geoCallback, onError);
+}
+
+// The callback function must catch the object with the position
+function geoCallback(position){
+
+    // Printing the position object to the console
+    console.log(position);
+
+    // Extracting the latitude and longitude
+    // from the position object
+    var lat = position.coords.latitude;
+    var lon = position.coords.longitude;
+
+    // Formatting the data to put it on the front end
+    var location = "Lat: " + lat + "<br>Long: " + lon;
+
+    // Placing the data on the front end
+    document.getElementById('position').innerHTML = location;
+}
+
+// This is the callback function for the google maps API
+function initMap() {
+
+    // Defining a position to display
+    var cct = {lat: 53.346, lng: -6.2588};
+    
+    // Creating the map, centred on the position 
+    // defined above
+    var myMap = new google.maps.Map(document.getElementById('map'),
+        {zoom: 18,
+        center: cct });
+    
+    // Creating a marker to place on the map
+    // at the position defined above
+    var marker = new google.maps.Marker(
+        { position: cct,
+        map: myMap });
+    
+    // Adding another pointer
+    var otherLocation = {lat: 53.3458, lng: -6.2575};
+    var marker2 = new google.maps.Marker(
+        { position: otherLocation,
+        map: myMap });
+
+    // REMEMBER: I added some style to the style file
+    // to be able to display the map!!!
+         
+} 
+
+// This function is going to be in charge of invoking
+// the open cage external API
+function openCage(){
+
+    // The XMLHttpRequest object, is the one in 
+    // charge of handleing the request for us
+    var http = new XMLHttpRequest();
+
+    // The url to send the request to. Notice that we're passing
+    // here some value of Latituted and longitude for the API 
+    // to process
+    const url = 'https://api.opencagedata.com/geocode/v1/json?q=53.34592+-6.25881&key=22e5695431c543d682e4d4b52ec743ab';
+    // Opening the request. Remember, we will send
+    // a "GET" request to the URL define above
+    http.open("GET", url);
+    // Sending the request
+    http.send();
+
+    // Once the request has been processed and we have
+    // and answer, we can do something with it
+    http.onreadystatechange = (e) => {
+        
+        // First, I'm extracting the reponse from the 
+        // http object in text format
+        var response = http.responseText;
+
+        // As we know that answer is a JSON object,
+        // we can parse it and handle it as such
+        var responseJSON = JSON.parse(response); 
+    
+        // Printing the result JSON to the console
+        console.log(responseJSON);
+
+        // Extracting the individual values, just as we
+        // do with any JSON object. Just as we did 
+        // with the position.
+        // REMEMBER: In this case, we have an array inside 
+        // the JSON object.
+        var city = responseJSON.results[0].components.city;
+        var country = responseJSON.results[0].components.country;
+        var currency = responseJSON.results[0].annotations.currency.name;
+
+        // Formattng data to put it on the front end
+        var oc = "City: " + city + "<br>Country: " + country + "<br>Currency: " + currency;
+
+        // Placing formatted data on the front ed
+        document.getElementById('opencage').innerHTML = oc;
     }
-};
+    
+}
+
