d893ebbe71a907d4e8787010e8cefd85212fee5d
scaldy010 <47572824+scaldy010@users.noreply.github.com>
2019-04-23 20:03:38 +0100

    test

--- platforms/browser/www/js/GPS.js
+++ platforms/browser/www/js/GPS.js
@@ -1,50 +1,25 @@
 // Initialize app
 var myApp = new Framework7();
-
-
-
 var $$ = Dom7;
-
-
 var mainView = myApp.addView('.view-main', {
     // Because we want to use dynamic navbar, we need to enable it for this view:
     dynamicNavbar: true
 });
-
 // Handle Cordova Device Ready Event
 $$(document).on('deviceready', function() {
     console.log("Device is ready!");
 
 });
 
+function openCage(position){
+    alert("got to open cage");
 
-function getLocation(){
-  
-    navigator.geolocation.getCurrentPosition(geoCallback, onError);
-}
-
-
-function geoCallback(position){
-
-  
-    console.log(position);
-
-  
     var lat = position.coords.latitude;
     var lon = position.coords.longitude;
-
-  
-    var location = "Lat: " + lat + "<br>Long: " + lon;
-
-    document.getElementById('position').innerHTML = location;
-}
-function openCage(){
-
- 
     var http = new XMLHttpRequest();
 
    
-    const url = 'https://api.opencagedata.com/geocode/v1/json?q=53.34592+-6.25881&key=22e5695431c543d682e4d4b52ec743ab';
+    const url = 'https://api.opencagedata.com/geocode/v1/json?q='+lat+'+'+lon+'&key=b4d8a0a740ea4821b56bcb4b53fb8750';
   
     http.open("GET", url);
    
@@ -65,9 +40,11 @@ function openCage(){
         var city = responseJSON.results[0].components.city;
         var country = responseJSON.results[0].components.country;
         var currency = responseJSON.results[0].annotations.currency.name;
+   
+        
 
        
-        var oc = "City: " + city + "<br>Country: " + country + "<br>Currency: " + currency;
+        var oc = "City : " + city + "<br>Country : " + country + "<br>Currency : " + currency;
 
     
         document.getElementById('opencage').innerHTML = oc;
--- www/js/app.js
+++ www/js/app.js
@@ -1,11 +1,12 @@
-function startApp(){
+function startApp() 
+{
   alert("got to start app");
   //Placeholders
   var lat, lon;
 
   console.log("Start");
   //Get current position
-  navigator.geolocation.getCurrentPosition(function(position) {
+  navigator.geolocation.getCurrentPosition(function (position) {
     console.log("Pos", position);
     // Save current position
     lat = position.coords.latitude;
@@ -13,52 +14,50 @@ function startApp(){
     openCage(position);
     // Once the request has been processed and we have
     // and answer, we can do something with it
-   
-    
-  }, function(err) {
+
+
+  }, function (err) {
     // Handle some error
     console.log('err', err);
   });
 }
 
-function openCage(position)
-{
-  alert("got to open cage");
+// function openCage(position) {
+//   alert("got to open cage");
 
-  var lat = position.coords.latitude;
-  var lon = position.coords.longitude;
-  var http = new XMLHttpRequest();
-
- 
-  const url = 'https://api.opencagedata.com/geocode/v1/json?q='+lat+'+'+lon+'&key=b4d8a0a740ea4821b56bcb4b53fb8750';
-
-  http.open("GET", url);
- 
-  http.send();
+//   var lat = position.coords.latitude;
+//   var lon = position.coords.longitude;
+//   var http = new XMLHttpRequest();
 
 
-  http.onreadystatechange = (e) => 
-  {
-      
-    
-      var response = http.responseText;
+//   const url = 'https://api.opencagedata.com/geocode/v1/json?q=' + lat + '+' + lon + '&key=b4d8a0a740ea4821b56bcb4b53fb8750';
 
-     
-      var responseJSON = JSON.parse(response); 
-  
-      console.log(responseJSON);
+//   http.open("GET", url);
+
+//   http.send();
 
 
-      var city = responseJSON.results[0].components.city;
-      var country = responseJSON.results[0].components.country;
-      var currency = responseJSON.results[0].annotations.currency.name;
- 
-      
+//   http.onreadystatechange = (e) => {
 
-     
-      var oc = "City : " + city + "<br>Country : " + country + "<br>Currency : " + currency;
 
-  
-      document.getElementById('opencage').innerHTML = oc;
-  }
-}
+//     var response = http.responseText;
+
+
+//     var responseJSON = JSON.parse(response);
+
+//     console.log(responseJSON);
+
+
+//     var city = responseJSON.results[0].components.city;
+//     var country = responseJSON.results[0].components.country;
+//     var currency = responseJSON.results[0].annotations.currency.name;
+
+
+
+
+//     var oc = "City : " + city + "<br>Country : " + country + "<br>Currency : " + currency;
+
+
+//     document.getElementById('opencage').innerHTML = oc;
+//   }
+// }
